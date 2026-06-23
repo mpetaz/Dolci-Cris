e79c77ed02c8e8fa54da0b8a9d5eb7009f84cd76
Moreno <Moreno@Air-di-Moreno.fritz.box>
2026-06-23 22:41:57 +0200

    Simplify to a single index.html static PWA app for GitHub Pages

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Dashboard from './Dashboard';
-
-export default function Home() {
-  return (
-    <div className="flex-1 flex flex-col bg-slate-50 min-h-screen">
-      <Dashboard />
-    </div>
-  );
-}
